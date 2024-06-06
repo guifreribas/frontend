5273d0d253aa71f9f19081cf8ea2317a9a79b678
Guifré Puig Ribas <gpuig@brothapps.com>
2024-06-06 10:05:03 +0200

    Exercici 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MapaComponent } from './pages/mapa/mapa.component';
+import { FullCalendarComponent } from './pages/full-calendar/full-calendar.component';
+import { GraficsComponent } from './pages/grafics/grafics.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'map', component: MapaComponent },
+  { path: 'full-calendar', component: FullCalendarComponent },
+  { path: 'graphics', component: GraficsComponent },
+  { path: '**', redirectTo: '' },
+];
